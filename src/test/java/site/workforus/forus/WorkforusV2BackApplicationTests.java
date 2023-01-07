7e513e981ef30b5fa5d433ad58e7c54e01146236
최영원 <youngwon0607@gmail.com>
2023-01-07 12:47:09 +0900

    test : test profile 추가

--- src/test/java/site/workforus/forus/WorkforusV2BackApplicationTests.java
+++ src/test/java/site/workforus/forus/WorkforusV2BackApplicationTests.java
@@ -2,7 +2,9 @@ package site.workforus.forus;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class WorkforusV2BackApplicationTests {
 
